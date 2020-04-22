d4d73bc9ad587349eeb1869a4d719b1dd7108f19
Gauthamjm007 <j.mgautham15@gmail.com>
2020-04-22 14:48:36 +0530

    adding heroku

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ setUpDb();
 app.use(express.json());
 app.use("/", router);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("listening on port ", port);
