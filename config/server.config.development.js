a23a8d0ff8cc07794141046818f73c527b998d72
Gauthamjm007 <j.mgautham15@gmail.com>
2020-04-22 15:22:32 +0530

    adding messages

---
+++ config/server.config.development.js
@@ -0,0 +1,12 @@
+module.exports = {
+  twitterConfig: {
+    consumer_key: "HlEmMglQgaUlXdsi0f3n7Bmsy",
+    consumer_secret: "QfNbOMIkraaS700ogUbjqrPYbLQ2PcHntAB4KwlhD4p4JXkUXN",
+    access_token_key: "1239493661286531072-Ya7jfJMeRqsQwzbNeg3AXt9FGoUuV6",
+    access_token_secret: "1aFNEnSba7sdxrT5b6D3uQ6K7R6IsCNu4MQC9F9c5SdU8",
+  },
+  mongoDBKey: {
+    uri:
+      "mongodb+srv://gouthamjm:secret123@cluster0-dtoso.mongodb.net/test?retryWrites=true&w=majority",
+  },
+};
