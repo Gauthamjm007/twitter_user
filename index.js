fdd97fa97e2eac49a061a83faf7199030121860d
Gauthamjm007 <j.mgautham15@gmail.com>
2020-04-22 14:09:39 +0530

    adding the build

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ setUpDb();
 app.use(express.json());
 app.use("/", router);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log("listening on port ", port);
 });
