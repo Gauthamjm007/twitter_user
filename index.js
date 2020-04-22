69de5fdcb58dfabc591797e99c9e6059ced5f018
Gauthamjm007 <j.mgautham15@gmail.com>
2020-04-22 15:14:42 +0530

    adding changes

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ setUpDb();
 app.use(express.json());
 app.use("/", router);
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
 app.listen(port, () => {
   console.log("listening on port ", port);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
